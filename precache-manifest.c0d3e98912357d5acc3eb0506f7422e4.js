915307c5e34dbdbe7b93ba11fd5f290dcc9186d4
Edwin Noriega <adolfonoriega9@gmail.com>
2020-04-18 21:59:07 -0600

    Updates

---
+++ precache-manifest.c0d3e98912357d5acc3eb0506f7422e4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb938cb4b15ccd46beca",
+    "url": "/react-badges/static/css/main.52b8d1ff.chunk.css"
+  },
+  {
+    "revision": "cb938cb4b15ccd46beca",
+    "url": "/react-badges/static/js/main.cb938cb4.chunk.js"
+  },
+  {
+    "revision": "1ca7e770b323ef47f898",
+    "url": "/react-badges/static/js/runtime~main.1ca7e770.js"
+  },
+  {
+    "revision": "481614a7c9db2a6eb687",
+    "url": "/react-badges/static/css/2.fb036aba.chunk.css"
+  },
+  {
+    "revision": "481614a7c9db2a6eb687",
+    "url": "/react-badges/static/js/2.481614a7.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/react-badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/react-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/react-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/react-badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "c8acf534f6cdc4caf1205adf03dd16b7",
+    "url": "/react-badges/index.html"
+  }
+];
